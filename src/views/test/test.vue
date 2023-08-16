a710ffa5c8094d6c3bf9ba56ed9dfb2b1c669a1a
阿惹 <522396150@qq.com>
2023-08-16 18:24:10 +0800

    初始化

---
+++ src/views/test/test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      testGroup: {
+        name: "test",
+        age: 25,
+      },
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        const res = await _getData();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    init(){
+        const test ={
+            name:'TEST',
+            age:34
+        }
+      Object.assign(this.testGroup,test)
+      console.log(this.testGroup);
+    },
+  },
+  mounted(){
+        this.init()
+    }
+};
+</script>
+
+<style>
+</style>
